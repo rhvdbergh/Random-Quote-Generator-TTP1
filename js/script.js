dc94024ef0a80a212514b2543f9b9aba526b0b04
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-01-30 17:00:16 -0500

    Example quotes added

--- js/script.js
+++ js/script.js
@@ -3,8 +3,35 @@
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 // array to hold quote objects
-// quote objects framework: { quote: '', source: '', citation: '', year: '' }
-var quotes = [{}, {}];
+// quote objects framework: { quote: '', source: '', citation: '', year: '', bartlettPage: '' }
+// quotations sourced from "Familiar Quotations" by John Bartlett, 1968 edition, available online
+// at https://archive.org/details/familiarquotatio017007mbp
+var quotes = [{
+        quote: 'To different minds, the same world is a hell, and a heaven',
+        source: 'Ralph Waldo Emerson',
+        citation: 'Journal',
+        year: '1822',
+        bartlettPage: '604'
+    },
+    {
+        quote: 'One man, one vote.',
+        source: 'Civil rights slogan',
+        bartlettPage: '1104'
+    },
+    {
+        quote: 'Death is nothing to us, since when we are, death has not come, and when death has come, we are not.',
+        source: 'Epicurus',
+        citation: 'Diogenes Laertius 10.125',
+        bartlettPage: '103'
+    },
+    {
+        quote: 'Whatever is worth doing at all, is worth doing well.',
+        source: 'Philip Dormer Stanhope',
+        citation: 'Letters',
+        year: '1746',
+        bartlettPage: '415'
+    }
+];
 
 // function to select and return quote object randomly from quotes array 
 function getRandomQuote() {
